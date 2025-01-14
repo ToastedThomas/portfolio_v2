6b72f07f96fe40adc0e2e16f3aad5800b1455a5d
ToastedThomas <blaketeahelser@gmail.com>
2025-01-14 11:04:50 -0600

    chore: relocate projects info to separate file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 import React, { useState } from "react";
 import ProjectTile from "./components/ProjectTile";
 import Modal from "./components/Modal";
+import projects from "./data/ProjectsInfo";
 
-const projects = [
-  {
-    name: "Pachinko Builder",
-    image: "/portfolio_v2/assets/images/tiles/pachinkoBuilder.png"
-  },
-  {
-    name: "Better Exchange",
-    image: "/portfolio_v2/assets/images/tiles/betterExchange.png"
-  },
-  {
-    name: "Crop Clicker",
-    image: "/portfolio_v2/assets/images/tiles/cropClicker.png"
-  },
-  {
-    name: "Falling Sand",
-    image: "/portfolio_v2/assets/images/tiles/fallingSand.png"
-  },
-  {
-    name: "Sample Survey",
-    image: "/portfolio_v2/assets/images/tiles/sampleSurvey.png"
-  },
-  {
-    name: "Dice Roller",
-    image: "/portfolio_v2/assets/images/tiles/diceRoller.png"
-  },
-  {
-    name: "PokeInfo",
-    image: "/portfolio_v2/assets/images/tiles/pokeInfo.png"
-  },
-]
 
 const App = () => {
   const [isModalOpen, setModalOpen] = useState(false);
---
+++ src/data/ProjectsInfo.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    name: "Pachinko Builder",
+    image: "/portfolio_v2/assets/images/tiles/pachinkoBuilder.png"
+  },
+  {
+    name: "Better Exchange",
+    image: "/portfolio_v2/assets/images/tiles/betterExchange.png"
+  },
+  {
+    name: "Crop Clicker",
+    image: "/portfolio_v2/assets/images/tiles/cropClicker.png"
+  },
+  {
+    name: "Falling Sand",
+    image: "/portfolio_v2/assets/images/tiles/fallingSand.png"
+  },
+  {
+    name: "Sample Survey",
+    image: "/portfolio_v2/assets/images/tiles/sampleSurvey.png"
+  },
+  {
+    name: "Dice Roller",
+    image: "/portfolio_v2/assets/images/tiles/diceRoller.png"
+  },
+  {
+    name: "PokeInfo",
+    image: "/portfolio_v2/assets/images/tiles/pokeInfo.png"
+  },
+];
+
+export default projects;
